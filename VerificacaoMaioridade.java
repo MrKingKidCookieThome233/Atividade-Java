db40a565330430367dd616654cd2e5ae68c6533c
Thomé da Silva Solon <118899452+MrKingKidCookieThome233@users.noreply.github.com>
2026-05-19 23:16:42 -0300

    Update and rename VerificacaoMaioridade to VerificacaoMaioridade.java

---
+++ VerificacaoMaioridade.java
@@ -0,0 +1,13 @@
+package com.pratica;
+
+public class VerificacaoMaioridade {
+    public static void main(String[] args) {
+        int idade = 20;
+
+        if (idade >= 18) {
+            System.out.println("Você é MAIOR de idade.");
+        } else {
+            System.out.println("Você é MENOR de idade.");
+        }
+    }
+}
